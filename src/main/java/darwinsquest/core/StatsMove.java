a7b8fdecd38a77ee3e88e87a91448e3db708cc73
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2023-04-22 20:08:51 +0200

    Fixed build errors

--- src/main/java/darwinsquest/core/StatsMove.java
+++ src/main/java/darwinsquest/core/StatsMove.java
@@ -4,7 +4,7 @@ package darwinsquest.core;
  * Interface that represents a {@link Move} that can change the stats of a {@link Banion}.
 */
 public interface StatsMove extends Move {
-    
+
     /*Statistic computeDelta(Statistic stat);
      * This method will be private in a implementation of this interface.
     */
