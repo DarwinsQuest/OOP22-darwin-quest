4d429aab7470adb1bccb6dfbe2c9366d06a2441d
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-04-23 17:13:05 +0200

    wip: Die implementation

---
+++ src/main/java/darwinsquest/core/Die.java
@@ -0,0 +1,52 @@
+package darwinsquest.core;
+
+import java.util.Random;
+import java.util.function.IntSupplier;
+import java.util.random.RandomGenerator;
+
+/**
+ * A polyhedral die implementation of an {@link IntSupplier}.
+ * @see <a href="https://en.wikipedia.org/wiki/Dice#Polyhedral_dice">Polyhedral die</a>.
+ */
+public class Die implements IntSupplier {
+
+    private static final int DEFAULT_FACES = 6;
+    private static final int MIN_FACES = 4;
+    private final int faces;
+    private final RandomGenerator generator;
+
+    /**
+     * Default constructor.
+     */
+    public Die() {
+        generator = new Random();
+        faces = DEFAULT_FACES;
+    }
+
+    /**
+     * Custom constructor for the class {@link Die}.
+     * This constructor allows the instantiation of strictly
+     * polyhedral dice.
+     * @param faces the amount of the custom {@link Die}'s faces.
+     */
+    public Die(final int faces) throws IllegalStateException {
+        generator = new Random();
+        if (!isDieLegal(faces)) {
+            throw new IllegalStateException("Not a platonic solid.");
+        }
+        this.faces = faces;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getAsInt() {
+        return generator.nextInt(faces) + 1;
+    }
+
+    private boolean isDieLegal(final int faces) {
+        return faces >= MIN_FACES && faces % 2 == 0;    // A die with an odd number of faces is not a polyhedron.
+    }
+
+}
