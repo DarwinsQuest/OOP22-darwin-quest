52608d0dbf7e95822dd01c1319124ac9b3a0831c
Enrico Marchionni <enrico.marchionni@studio.unibo.it>
2023-04-24 15:04:03 +0200

    Update Banion with Move management

--- src/main/java/darwinsquest/core/Banion.java
+++ src/main/java/darwinsquest/core/Banion.java
@@ -1,5 +1,7 @@
 package darwinsquest.core;
 
+import java.util.Collection;
+
 /**
  * Interface that represents a game monster.
  */
@@ -10,6 +12,26 @@ public interface Banion {
      * @return if is alive or not.
      */
     boolean isAlive();
+
+    /**
+     * Retrieves moves that can be performed.
+     * @return Moves that can be performed.
+     */
+    Collection<Move> getMoves();
+
+    /**
+     * Adds a {@link Move} to the possible moves to perform.
+     * @param move A move.
+     * @return {@code true} if move added correctly.
+     */
+    boolean learnMove(Move move);
+
+    /**
+     * Removes a specific move.
+     * @param move The move that will be removed.
+     * @return If move removed correctly.
+     */
+    boolean forgetMove(Move move);
 
     /**
      * Provides the life stat amount.
