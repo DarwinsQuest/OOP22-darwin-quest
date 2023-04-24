448082e880a4a539048e18d678155ea823247427
Enrico Marchionni <enrico.marchionni@studio.unibo.it>
2023-04-24 14:58:34 +0200

    Update Board and Banion after enviroment review

--- src/main/java/darwinsquest/core/BanionImpl.java
+++ src/main/java/darwinsquest/core/BanionImpl.java
@@ -3,28 +3,20 @@ package darwinsquest.core;
 import java.util.function.IntPredicate;
 
 /**
- * Class that implements a {@link Banion}.
+ * Class that implements a basic {@link Banion}.
  */
 public class BanionImpl implements Banion {
 
     private int hp;
-    private int atk;
-    private int def;
 
     /**
-     * Costructor that creates a {@link BanionImpl}.
+     * Costructor that creates a simple {@link Banion} with a provided hit points amount.
      * @param hp Hit points, represents health.
-     * @param atk Attack, represents damage tendency.
-     * @param def Defense, represents resistence hit tendency.
-     * @throws IllegalArgumentException Stats can't be init as negative or zero.
+     * @throws IllegalArgumentException Hit points can't be init as negative or zero.
      */
-    public BanionImpl(final int hp, final int atk, final int def) {
-        if (hp <= 0 || atk <= 0 || def <= 0) {
-            throw new IllegalArgumentException("Stats can't be init as negative or zero.");
-        }
+    public BanionImpl(final int hp) {
+        testStat(hp, value -> value > 0, "Banion hp can't be init to a negative value or zero.");
         this.hp = hp;
-        this.atk = atk;
-        this.def = def;
     }
 
     private void testStat(final int stat, final IntPredicate predicate, final String message) {
@@ -47,44 +39,8 @@ public class BanionImpl implements Banion {
      */
     @Override
     public void setHp(final int amount) {
-        testStat(amount, hp -> hp >= 0, "Banion hp can't be set to a value < 0.");
+        testStat(amount, value -> value >= 0, "Banion hp can't be set to a negative value.");
         hp = amount;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int getAtk() {
-        return atk;
-    }
-
-    /**
-     * {@inheritDoc}
-     * @throws IllegalArgumentException If attack set to negative or zero.
-     */
-    @Override
-    public void setAtk(final int amount) {
-        testStat(amount, atk -> atk > 0, "Banion attack can't be set to a value <= 0.");
-        atk = amount;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int getDef() {
-        return def;
-    }
-
-    /**
-     * {@inheritDoc}
-     * @throws IllegalArgumentException If defense set to negative or zero.
-     */
-    @Override
-    public void setDef(final int amount) {
-        testStat(amount, def -> def > 0, "Banion defense can't be set to a value <= 0.");
-        def = amount;
     }
 
     /**
