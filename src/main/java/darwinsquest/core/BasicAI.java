da71e3284a6465efd6816fead280278dfcf83190
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2023-05-03 20:17:46 +0200

    Added BasicAI with related tests

---
+++ src/main/java/darwinsquest/core/BasicAI.java
@@ -0,0 +1,55 @@
+package darwinsquest.core;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
+/**
+ * A basic implementation of the game AI.
+ */
+public class BasicAI implements AI {
+
+    private final RandomGenerator generator;
+
+    /**
+     * Default constructor.
+     */
+    public BasicAI() {
+        generator = new Random();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Banion decideBanionDeployment(final Collection<Banion> banions) {
+        return getRandomElement(banions);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Move decideMoveSelection(final Collection<Move> moves) {
+        return getRandomElement(moves);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Optional<Banion> decideBanionSwap(final Collection<Banion> banions) {
+        final Collection<Banion> remainingBanions = banions.stream().filter(banion -> banion.isAlive()).toList();
+        if (remainingBanions.isEmpty()) {
+            return Optional.empty();
+        } else {
+            return Optional.of(getRandomElement(remainingBanions));
+        } 
+    }
+
+    private <T> T getRandomElement(final Collection<T> coll) {
+        return coll.stream().toList().get(generator.nextInt(coll.size()));
+    }
+
+}
