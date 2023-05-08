79a8b27602c11d91df44cb4da8cff77b507c7eff
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-05-09 00:28:48 +0200

    Updated PlayerImpl, OpponentImpl and related tests to extend AbstractEntity

--- src/main/java/darwinsquest/core/PlayerImpl.java
+++ src/main/java/darwinsquest/core/PlayerImpl.java
@@ -1,20 +1,12 @@
 package darwinsquest.core;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * Class that represents the {@link Player} implementation.
  */
-public class PlayerImpl implements Player {
-
-    private final String nickname;
-    private final List<Banion> inventory = new LinkedList<>();
+public class PlayerImpl extends AbstractEntity implements Player {
 
     /**
      * The player constructor.
@@ -37,34 +29,7 @@ public class PlayerImpl implements Player {
      * @param nickname the player's nickname.
      */
     public PlayerImpl(final String nickname) {
-        if (Objects.isNull(nickname) || nickname.isBlank()) {
-            throw new IllegalArgumentException("Player nickname cannot be null or blank.");
-        }
-        final Pattern allowedPattern = Pattern.compile("^[a-zA-Z](?:[a-zA-Z0-9_]*[a-zA-Z0-9])?$");
-        final Matcher matcher = allowedPattern.matcher(nickname);
-        if (!matcher.find()) {
-            throw new IllegalArgumentException("Invalid nickname format.");
-        }
-        this.nickname = nickname;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public List<Banion> getInventory() {
-        return Collections.unmodifiableList(inventory);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void updateInventory(final int index, final Banion banion) {
-        if (index >= inventory.size()) {
-            inventory.add(index, banion);
-        }
-        inventory.set(index, banion);
+        super(nickname, Pattern.compile("^[a-zA-Z](?:[a-zA-Z0-9_]*[a-zA-Z0-9])?$"));
     }
 
     /**
@@ -95,49 +60,10 @@ public class PlayerImpl implements Player {
      * {@inheritDoc}
      */
     @Override
-    public boolean isOutOfBanions() {
-        return inventory.stream().noneMatch(Banion::isAlive);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        final PlayerImpl player = (PlayerImpl) o;
-        return Objects.equals(nickname, player.nickname) && Objects.equals(inventory, player.inventory);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int hashCode() {
-        return Objects.hash(nickname, inventory);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
     public String toString() {
         return "PlayerImpl{"
-                + "nickname='" + nickname + '\''
-                + ", inventory=" + inventory + '}';
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public String getName() {
-        return nickname;
+                + "nickname='" + getName() + '\''
+                + ", inventory=" + getInventory() + '}';
     }
 
 }
