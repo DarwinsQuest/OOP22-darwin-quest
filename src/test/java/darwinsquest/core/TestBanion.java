c3238be54307f4255c2ea526ec00a48c9b3ae836
Enrico Marchionni <enrico.marchionni@studio.unibo.it>
2023-05-13 15:58:25 +0200

    Add clone concept in Banion

--- src/test/java/darwinsquest/core/TestBanion.java
+++ src/test/java/darwinsquest/core/TestBanion.java
@@ -3,14 +3,17 @@ package darwinsquest.core;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import darwinsquest.core.element.Air;
 import darwinsquest.core.element.Fire;
@@ -69,7 +72,8 @@ class TestBanion {
             new BasicMove(damage, "neutralMove", new Neutral()),
             new BasicMove(damage, "airMove", new Air()));
 
-        moves.stream().forEach(move -> banion.learnMove(move));
+        moves.stream().forEach(banion::learnMove);
+
         assertTrue(banion.getMoves().stream()
             .collect(Collectors.toSet())
             .containsAll(moves.stream()
@@ -80,22 +84,28 @@ class TestBanion {
 
     /**
      * Tests {@link BanionImpl#equals}.
-     * @param hp the hit points of the {@link BanionImpl}.
+     * @param size the number of {@link BanionImpl} to create.
      */
     @ParameterizedTest
-    @CsvSource({"100"})
-    void comparisons(final int hp) {
-        final String name1 = "1";
-        final String name2 = "2";
-        final var banion1 = new BanionImpl(new Water(), name1, hp);
-        final var banion2 = new BanionImpl(new Water(), name1, hp);
-        final var banion3 = new BanionImpl(new Fire(), name1, hp);
-        final var banion4 = new BanionImpl(new Neutral(), name1, hp);
-        final var banion5 = new BanionImpl(new Water(), name2, hp);
+    @ValueSource(ints = {10, 100, 1_000})
+    void comparisons(final long size) {
+        final var name = "Translucent";
+        final var hp = 100;
+        final var element = new Fire();
 
-        assertEquals(banion1, banion2);
-        assertNotEquals(banion1, banion3);
-        assertNotEquals(banion1, banion4);
-        assertNotEquals(banion4, banion5);
+        final var banions = Stream.generate(() -> new BanionImpl(element, name, hp)).limit(size).toList();
+
+        banions.forEach(b1 -> {
+            banions.stream().filter(b2 -> b1 != b2).forEach(b3 -> assertNotEquals(b1, b3));
+            banions.stream().filter(b2 -> b1 == b2).forEach(b3 -> assertEquals(b1, b3));
+        });
+
+        final var banion = new BanionImpl(element, name, hp);
+        final var identifiables = Stream.generate(banion::copy).limit(size).toList();
+
+        identifiables.forEach(o1 -> identifiables.forEach(o2 -> {
+            assertNotSame(banion, o2);
+            assertEquals(o1, o2);
+        }));
     }
 }
