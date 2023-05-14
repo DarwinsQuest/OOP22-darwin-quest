83777575366b9a7e78ea276bf9207682a2041535
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2023-05-14 13:04:33 +0200

    Added decision mechanism with related tests

---
+++ src/main/java/darwinsquest/core/decision/Decision.java
@@ -0,0 +1,9 @@
+package darwinsquest.core.decision;
+
+/**
+ * Interface that represents a decision made by a {@link darwinsquest.core.Entity}
+ * during a battle.
+ */
+public interface Decision {
+
+}
