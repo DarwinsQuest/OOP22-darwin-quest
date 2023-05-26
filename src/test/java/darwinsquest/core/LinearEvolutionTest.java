f87d8fde288d39ee75831e01844dd289241419e5
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-05-27 01:18:42 +0200

    Improved evolveToLevel multimap implementation: fixed bugs, made it more strict

--- src/test/java/darwinsquest/core/LinearEvolutionTest.java
+++ src/test/java/darwinsquest/core/LinearEvolutionTest.java
@@ -25,6 +25,8 @@ class LinearEvolutionTest {
     public static final int LEVEL_5 = 5;
     public static final int LEVEL_6 = 6;
     public static final int LEVEL_7 = 7;
+    private static final int LEVEL_8 = 8;
+    private static final int LEVEL_9 = 9;
     public static final int LEVEL_10 = 10;
     public static final int LEVEL_11 = 11;
     public static final int LEVEL_15 = 15;
@@ -85,32 +87,51 @@ class LinearEvolutionTest {
 
     @Test
     void evolveToLevelMultiMapTest() {
-        final Banion b1 = new BanionImpl(neutral, NAME, DEFAULT_HP, moves);
+        final Banion b = new BanionImpl(neutral, NAME, DEFAULT_HP, moves);
         final MultiValuedMap<Predicate<Banion>, Integer> requirements = new ArrayListValuedHashMap<>();
+        // From lvl 1 to lvl 7.
+        requirements.putAll(banion -> banion.getLevel() <= LEVEL_3, List.of(LEVEL_1, LEVEL_2, LEVEL_3));
+        requirements.putAll(banion -> banion.getLevel() != LEVEL_11, List.of(LEVEL_4, LEVEL_5, LEVEL_6));
+        lastRecord = new BanionStats(LEVEL_7,
+                addPercentage(DEFAULT_RECORD.hp(), LEVEL_7 - 1),
+                addPercentage(DEFAULT_RECORD.maxHp(), LEVEL_7 - 1));
+        assertTrue(b.evolveToLevel(LEVEL_7, requirements));
+        assertRecordEquals(lastRecord, b);
+        // From lvl 7 to lvl 9.
+        requirements.putAll(banion -> true, List.of(LEVEL_7, LEVEL_8));
+        lastRecord = new BanionStats(LEVEL_9,
+                addPercentage(lastRecord.hp(), LEVEL_9 - LEVEL_7),
+                addPercentage(lastRecord.maxHp(), LEVEL_9 - LEVEL_7));
+        assertTrue(b.evolveToLevel(LEVEL_9, requirements));
+        assertRecordEquals(lastRecord, b);
+        // From lvl 9 to lvl 11.
+        requirements.putAll(banion -> true, List.of(LEVEL_9, LEVEL_10));
+        lastRecord = new BanionStats(LEVEL_11,
+                addPercentage(lastRecord.hp(), LEVEL_11 - LEVEL_9),
+                addPercentage(lastRecord.maxHp(), LEVEL_11 - LEVEL_9));
+        assertTrue(b.evolveToLevel(LEVEL_11, requirements));
+        assertRecordEquals(lastRecord, b);
+    }
+
+    @Test
+    void multiMapErrorsTest() {
+        final Banion b = new BanionImpl(neutral, NAME, DEFAULT_HP, moves);
+        final MultiValuedMap<Predicate<Banion>, Integer> requirements = new ArrayListValuedHashMap<>();
+        // Duplicates test.
         requirements.putAll(banion -> true, List.of(LEVEL_1, LEVEL_1, LEVEL_2));
-        assertThrows(IllegalArgumentException.class, () -> b1.evolveToLevel(LEVEL_3, requirements));
+        assertThrows(IllegalArgumentException.class, () -> b.evolveToLevel(LEVEL_3, requirements));
         requirements.clear();
+        // Map with gap test.
         requirements.putAll(banion -> banion.getLevel() <= LEVEL_2, List.of(LEVEL_1, LEVEL_2));
         requirements.putAll(banion -> banion.getLevel() % LEVEL_3 == 0, List.of(LEVEL_3, LEVEL_4, LEVEL_6));
-        assertFalse(b1.evolveToLevel(LEVEL_7, requirements));
-        assertRollback(DEFAULT_RECORD, b1);
+        assertThrows(IllegalArgumentException.class, () -> b.evolveToLevel(LEVEL_7, requirements));
         requirements.clear();
-        b1.evolveToLevel(LEVEL_3, banion -> true);
-        lastRecord = new BanionStats(LEVEL_3,
-                addPercentage(DEFAULT_RECORD.hp(), LEVEL_3 - 1),
-                addPercentage(DEFAULT_RECORD.maxHp(), LEVEL_3 - 1));
-        requirements.putAll(banion -> true, List.of(LEVEL_1, 2));
-        assertThrows(IllegalArgumentException.class, () -> b1.evolveToLevel(LEVEL_3, requirements));
-        assertRecordEquals(lastRecord, b1);
-        final var b2 = new BanionImpl(neutral, NAME, DEFAULT_HP, moves);
+        // Rollback test.
+        requirements.putAll(banion -> banion.getLevel() <= LEVEL_2, List.of(LEVEL_1, LEVEL_2));
+        requirements.putAll(banion -> banion.getLevel() % 3 == 0, List.of(LEVEL_3, LEVEL_4));
+        assertFalse(b.evolveToLevel(LEVEL_5, requirements));
+        assertRollback(DEFAULT_RECORD, b);
         requirements.clear();
-        requirements.putAll(banion -> banion.getLevel() <= LEVEL_3, List.of(LEVEL_1, LEVEL_2, LEVEL_3));
-        requirements.putAll(banion -> banion.getLevel() != LEVEL_11, List.of(LEVEL_4, LEVEL_6, LEVEL_10));
-        lastRecord = new BanionStats(LEVEL_11,
-                addPercentage(DEFAULT_RECORD.hp(), LEVEL_11 - 1),
-                addPercentage(DEFAULT_RECORD.maxHp(), LEVEL_11 - 1));
-        assertTrue(b2.evolveToLevel(LEVEL_11, requirements));
-        assertRecordEquals(lastRecord, b2);
     }
 
     private void assertRollback(final BanionStats record, final Banion banion) {
