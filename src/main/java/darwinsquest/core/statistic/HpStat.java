821908e8d976a5a5b614e46f0832eed9118e3e94
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2023-06-01 17:28:40 +0200

    Added attack and defence stat classes, sealed HpStat

--- src/main/java/darwinsquest/core/statistic/HpStat.java
+++ src/main/java/darwinsquest/core/statistic/HpStat.java
@@ -4,7 +4,7 @@ package darwinsquest.core.statistic;
  * Class that represents a health points (HP) statistic.
  * @param <T> the type of the value that numerically represents the statistic.
  */
-public class HpStat<T extends Number> extends AbstractStatistic<T> {
+public final class HpStat<T extends Number> extends AbstractStatistic<T> {
 
     /**
      * Constructor for a health point (HP) statistic.
